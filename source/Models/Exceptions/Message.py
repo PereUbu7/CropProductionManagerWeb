8326d14467634a4c18d1600e9280b3776641a3b5
Martin Noring <martin.noring@gmail.com>
2021-04-24 21:20:54 +0200

    First pieces of Crop

---
+++ source/Models/Exceptions/Message.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+""" Message in different languages
+"""
+
+class Message():
+    def __init__(self, swedish: str, english: str):
+        self.Swedish = swedish
+        self.English = english
---
+++ source/Program.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" Crop production manager starting point
+"""
+
+from Models.Core.Crop import Crop
+
+
+a = Crop()
+
+a.NumberOfPlants = -1
