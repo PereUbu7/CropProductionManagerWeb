05790dfd30b9f0f63798ee85725d5100287eacf1
Martin Noring <martin.noring@gmail.com>
2022-01-14 21:49:41 +0100

    More method handlers and posibility to get by id

--- src/Server/RequestHandler/crop.cpp
+++ src/Server/RequestHandler/crop.cpp
@@ -9,26 +9,45 @@ namespace CropProductionManager::Server::RequestHandler::CropMethod
 
     void Get(const shared_ptr<Session> session)
     {
-        std::cout << "Get request\n";
-        auto request = session->get_request();
+        try
+        {
+            std::cout << "Get request\n";
+            auto request = session->get_request();
 
-        std::cout << "Path: " << request->get_path() << "\n";
-        // Get actual data
-        namespace Infrastructure = CropProductionManager::InternalModel::Infrastructure;
+            std::cout << "Path: " << request->get_path() << "\n";
+            std::cout << "Has path id: " << request->has_path_parameter("id") << "\n";
+            std::cout << "Id: " << request->get_path_parameter("id", "0") << "\n";
 
-        CropProductionManager::Serializer::Serializer<std::vector<CropProductionManager::ModelApi::Crop>> serializer{};
+            int requestedId = std::stoi(request->get_path_parameter("id", "0"));
 
-        CropProductionManager::Core::CropCore core{implementationHolder->Repository};
-        CropProductionManager::Api::CropApi api{core};
+            // Get actual data
+            CropProductionManager::Core::CropCore core{implementationHolder->Repository};
+            CropProductionManager::Api::CropApi api{core};
+            std::string json;
 
-        auto crops{api.Get()};
-        auto json{serializer.Serialize(crops).GetJson().dump()};
+            if (request->has_path_parameter("id"))
+            {
+                CropProductionManager::Serializer::Serializer<CropProductionManager::ModelApi::Crop> serializer{};
+                auto crop{api.Get(requestedId)};
+                json = serializer.Serialize(crop).GetJson().dump();
+            }
+            else
+            {
+                CropProductionManager::Serializer::Serializer<std::vector<CropProductionManager::ModelApi::Crop>> serializer{};
+                auto crops{api.Get()};
+                json = serializer.Serialize(crops).GetJson().dump();
+            }
+            std::cout << json << '\n';
 
-        std::cout << json << '\n';
-
-        session->close(OK, json,
-                       {{"Content-Length", std::to_string(json.length())},
-                        {"Content-Type", "application/json"}});
+            session->close(OK, json,
+                           {{"Content-Length", std::to_string(json.length())}});
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            session->close(NOT_FOUND, "",
+                           {{"Content-Length", "0"}});
+        }
     }
 
     void Post(const shared_ptr<Session> session)
@@ -39,7 +58,7 @@ namespace CropProductionManager::Server::RequestHandler::CropMethod
         int content_length{};
         request->get_header("Content-Length", content_length, 0);
 
-        session->fetch(content_length, [](const shared_ptr<Session> session, const Bytes &body){});
+        session->fetch(content_length, [](const shared_ptr<Session> session, const Bytes &body) {});
 
         auto byteBody = request->get_body();
         std::cout << "body length: " << byteBody.size() << '\n';
@@ -54,13 +73,74 @@ namespace CropProductionManager::Server::RequestHandler::CropMethod
         std::cout << "Path: " << request->get_path() << "\n";
         std::cout << "Body: " << body << '\n';
         // Post actual data
-        namespace Infrastructure = CropProductionManager::InternalModel::Infrastructure;
-
         CropProductionManager::Serializer::Serializer<CropProductionManager::ModelApi::Crop> serializer{};
 
         CropProductionManager::Core::CropCore core{implementationHolder->Repository};
         CropProductionManager::Api::CropApi api{core};
 
         api.Post(serializer.Deserialize(body));
+    }
+
+    void Put(const shared_ptr<Session> session)
+    {
+        std::cout << "Put request\n";
+        auto request = session->get_request();
+
+        int content_length{};
+        request->get_header("Content-Length", content_length, 0);
+
+        session->fetch(content_length, [](const shared_ptr<Session> session, const Bytes &body) {});
+
+        auto byteBody = request->get_body();
+        std::cout << "body length: " << byteBody.size() << '\n';
+        string body(byteBody.begin(), byteBody.end());
+
+        for (auto c : byteBody)
+        {
+            std::cout << c;
+        }
+        std::cout << '\n';
+
+        std::cout << "Path: " << request->get_path() << "\n";
+        std::cout << "Has id: " << request->has_path_parameter("id") << "\n";
+        std::cout << "Body: " << body << '\n';
+        // Post actual data
+        CropProductionManager::Serializer::Serializer<CropProductionManager::ModelApi::Crop> serializer{};
+
+        CropProductionManager::Core::CropCore core{implementationHolder->Repository};
+        CropProductionManager::Api::CropApi api{core};
+
+        api.Put(serializer.Deserialize(body));
+    }
+
+    void Delete(const shared_ptr<Session> session)
+    {
+        std::cout << "Delete request\n";
+        auto request = session->get_request();
+
+        int content_length{};
+        request->get_header("Content-Length", content_length, 0);
+
+        session->fetch(content_length, [](const shared_ptr<Session> session, const Bytes &body) {});
+
+        auto byteBody = request->get_body();
+        std::cout << "body length: " << byteBody.size() << '\n';
+        string body(byteBody.begin(), byteBody.end());
+
+        for (auto c : byteBody)
+        {
+            std::cout << c;
+        }
+        std::cout << '\n';
+
+        std::cout << "Path: " << request->get_path() << "\n";
+        std::cout << "Body: " << body << '\n';
+        // Post actual data
+        CropProductionManager::Serializer::Serializer<CropProductionManager::ModelApi::Crop> serializer{};
+
+        CropProductionManager::Core::CropCore core{implementationHolder->Repository};
+        CropProductionManager::Api::CropApi api{core};
+
+        api.Put(serializer.Deserialize(body));
     }
 }
