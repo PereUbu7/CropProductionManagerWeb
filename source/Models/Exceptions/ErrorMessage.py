d0c20b09940b4a672960e03b1acb0551340da133
Martin Noring <martin.noring@gmail.com>
2022-03-02 10:33:35 +0100

    removed first prototype in python

--- source/Models/Exceptions/ErrorMessage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-""" Error Messages
-"""
-from Models.Exceptions.Message import Message
-
-NumberOfPlantsNotLowerThanZero = Message(swedish = "Plantantal kan inte vara mindre än 0", english = "Number of plants cannot be lower than 0")
-BatchNumberNotLowerThanZero = Message(swedish = "Omgång kan inte vara mindre än 1", english = "Batch cannot be lower than 1")
-GerminationRateOutsideOfRange = Message(swedish = "Grobarhet måste vara mellan 0 och 1", english = "Germination rate must be between 0 and 1")
--- source/Models/Exceptions/Message.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-""" Message in different languages
-"""
-
-class Message():
-    def __init__(self, swedish: str, english: str):
-        self.Swedish = swedish
-        self.English = english
