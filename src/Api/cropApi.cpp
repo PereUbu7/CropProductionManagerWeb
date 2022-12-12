057a29c2f88e0c10dd76d31a51f4fba0fef12253
Martin Noring <martin.noring@gmail.com>
2022-12-12 20:49:06 +0100

    cleaner code in cropApi

--- src/Api/cropApi.cpp
+++ src/Api/cropApi.cpp
@@ -19,7 +19,7 @@ namespace CropProductionManager::Api
     } 
     Crop CropApi::Get(const int id) const
     {
-        auto coreCrop = Core::Crop{_core.Get(id)};
+        auto coreCrop = _core.Get(id);
         return coreCrop.ToApi();
     }
     Crop CropApi::Post(const Crop& crop)
